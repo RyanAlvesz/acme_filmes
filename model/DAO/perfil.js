d0f48bae3c86f9ef5d1fe72a1822eba3e20dd3b2
RyanAlvesz <ra546038@gmail.com>
2024-04-20 23:42:53 -0300

    CRUD

--- model/DAO/perfil.js
+++ model/DAO/perfil.js
@@ -83,7 +83,7 @@ const selectByIdPerfil = async (id) => {
 const selectAllPerfisByIdUsuario = async (id) => {
 
     try {
-        let sql = `select tp.id, tp.apelido, tp.id_foto_perfil, tc.id as id_categoria_foto_perfil, tf.foto as foto_perfil, tf.nome as nome_foto, tc.nome as categoria_foto_perfil from tbl_perfil as tp inner join tbl_foto_perfil as tf on tp.id_foto_perfil=tf.id inner join tbl_categoria_foto_perfil as tc on tf.id_categoria_foto_perfil=tc.id where tp.id_usuario = ${id}`
+        let sql = `select tp.id, tp.apelido, tp.id_foto_perfil, tc.id as id_categoria_foto_perfil, tf.foto as foto_perfil, tf.nome as nome_foto, tc.nome as categoria_foto_perfil from tbl_perfil as tp inner join tbl_foto_perfil as tf on tp.id_foto_perfil=tf.id inner join tbl_categoria_foto_perfil as tc on tf.id_categoria_foto_perfil=tc.id where tp.id_usuario = ${id} order by tp.apelido asc`
         let rsPerfil = await prisma.$queryRawUnsafe(sql)
         return rsPerfil
     } catch (error) {
